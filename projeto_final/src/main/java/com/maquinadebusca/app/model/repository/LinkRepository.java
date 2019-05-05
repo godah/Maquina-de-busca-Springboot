a0e02a815e1417ef28415467cee3388ec6c676bf
LucianoLima <yoluciano.fernando@gmail.com>
2019-05-05 11:40:43 -0300

    import projeto-final

---
+++ projeto_final/src/main/java/com/maquinadebusca/app/model/repository/LinkRepository.java
@@ -0,0 +1,28 @@
+package com.maquinadebusca.app.model.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.maquinadebusca.app.model.Link;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+
+	@Override
+	List<Link> findAll();
+
+	Link findById(long id);
+
+	Link findByUrl(String url);
+
+	@Override
+	Link save(Link link);
+
+	@Query(value = "SELECT l.url FROM Link l WHERE l.ultimaColeta IS NULL ")
+	List<String> obterUrlsNaoColetadas();
+
+	List<Link> findByUrlIgnoreCaseContaining(String url);
+
+	@Query (value = "SELECT * FROM Link ORDER BY url", nativeQuery = true)
+	List<Link> getInLexicalOrder();
+}
